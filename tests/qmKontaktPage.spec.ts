f83e81903c446ff593ab63fac3b366c353bf0e18
tpr <Thorsten.Prehn@qualityminds.de>
2025-12-01 17:41:20 +0100

    fix

--- tests/qmKontaktPage.spec.ts
+++ tests/qmKontaktPage.spec.ts
@@ -8,12 +8,12 @@ test.describe('kontakt page tests', async () => {
   test('txt fields do exist (Dummy um Annotations zu demonstrieren)', {
     tag: '@dummyTag'
   }, async ({ qmKontaktPage }) => {
-    await expect(qmKontaktPage.txtKommentar).toBeVisible();
-    await expect(qmKontaktPage.txtKommentar).toBeVisible();
+    await expect(qmKontaktPage.txtNachricht).toBeVisible();
+    await expect(qmKontaktPage.txtNachricht).toBeVisible();
   });
 
   test('can go to contact page and read error messages', async ({ qmKontaktPage }) => {
-    await qmKontaktPage.txtKommentar.fill('TestKommentar');
+    await qmKontaktPage.txtNachricht.fill('TestKommentar');
     await qmKontaktPage.txtName.fill('Testname');
     await expect(qmKontaktPage.lblErrorMessageEMail).not.toBeVisible();
     await qmKontaktPage.btnSenden.click();
